a30a129cd6724ca608e9b6335527bbe6fa15570f
donghunee <ehdgns1766@naver.com>
2020-11-01 14:38:12 +0900

    first project

---
+++ src/modules/auth.js
@@ -0,0 +1,94 @@
+import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
+import { takeLatest } from "redux-saga/effects";
+import * as authAPI from "../lib/api/auth";
+import craeteRequestSaga from "../lib/createRequestSaga";
+
+const CHANGE_FIELD = "auth/SAMPLE_ACTION";
+const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
+
+const REGISTER = "auth/REGISTER";
+const REGISTER_SUCCESS = "auth/REGISTER_SUCCESS";
+const REGISTER_FAILURE = "auth/REGISTER_FAILURE";
+
+const LOGIN = "auth/LOGIN";
+const LOGIN_SUCCESS = "auth/LOGIN_SUCCESS";
+const LOGIN_FAILURE = "auth/LOGIN_FAILURE";
+
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form,
+    key,
+    value,
+  })
+);
+
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+
+export const register = createAction(REGISTER, ({ username, password }) => ({
+  username,
+  password,
+}));
+
+export const login = createAction(LOGIN, ({ username, password }) => ({
+  username,
+  password,
+}));
+
+const registerSaga = craeteRequestSaga(REGISTER, authAPI.register);
+const loginSaga = craeteRequestSaga(LOGIN, authAPI.login);
+
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
+
+const initState = {
+  register: {
+    username: "",
+    password: "",
+    passwordConfirm: "",
+  },
+  login: {
+    username: "",
+    password: "",
+  },
+  auth: null,
+  authError: null,
+};
+
+const auth = handleActions(
+  {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, action) => ({
+      ...state,
+      [action.payload.form]: initState[action.payload.form],
+      authError: null,
+    }),
+    [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [REGISTER_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
+    }),
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
+    }),
+  },
+  initState
+);
+
+export default auth;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <>
+      <div>메인 페이지</div>
+    </>
+  );
+};
+
+export default MainPage;
